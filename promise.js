43698434eb8a6c3975cead44cc00ffc0d2fe64a5
AndikaNFS <56013918+AndikaNFS@users.noreply.github.com>
2022-09-16 21:34:51 +0700

    UpdatePromise

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) =>  {
+  const checkTheaterIXX = await promiseTheaterIXX(emosi)
+  const checkTheaterVGC = await promiseTheaterVGC(emosi)
+
+  let penontonIXX = checkTheaterIXX.filter(function(element){
+    return element.hasil == emosi;
+  }).length
+
+  let penontonVGC = checkTheaterVGC.filter(function(element){
+    return element.hasil == emosi;
+  }).length
+
+  let penonton = penontonIXX + penontonVGC
+
+  return penonton 
+};
 
 module.exports = {
   promiseOutput,
